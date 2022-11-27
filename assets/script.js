9e9a16225d089f5c90274678ce0f1ef8c2385095
Yuliya-Lov <jul-yaris@yandex.ru>
2022-11-27 18:38:28 +0300

    Ховер эффекты, адаптация под экраны более 1200px

---
+++ assets/script.js
@@ -0,0 +1,48 @@
+
+function showModalWin() {
+    let darkWin = document.createElement("div");
+    let body = document.querySelector("body");
+    body.appendChild(darkWin);
+    darkWin.style.position="fixed";
+    darkWin.style.opacity="0.5";
+    darkWin.style.backgroundColor="#000";
+    darkWin.style.width="100%";
+    darkWin.style.height="100%";
+    darkWin.style.zIndex="1";
+    darkWin.style.left="0";
+    darkWin.style.top="0";
+    let modalWin = document.querySelector(".modal-win");
+    modalWin.style.display="flex";
+    modalWin.style.position="fixed";
+    modalWin.style.zIndex="2";
+    darkWin.onclick = function hideModalWin() {
+        darkWin.parentNode.removeChild(darkWin);
+        modalWin.style.display="none";
+        return false; 
+    } 
+}
+let startFeedback  = document.querySelector("#start-to-feedback");
+startFeedback.addEventListener ("click", () => {
+    showModalWin()
+})
+let thanksBlock  = document.querySelector(".thanks");
+let thanksText  = document.querySelector(".thanks-text");
+thanksText.textContent = "Thank you for  your opinion, my friend!";
+let OkButton  = document.querySelector(".ok-button");
+let SubmitButton = document.querySelector(".submit-modal-win");
+
+function showThanks() {
+    modalWin.style.display="none";
+    thanksBlock.style.zIndex ="2";
+    thanksBlock.style.display="block";
+    return false;
+}
+
+
+SubmitButton.addEventListener ("click", (evt) => {
+    evt.preventDefault();
+    showThanks()
+    }
+)
+
+
